7c48bf32e4ce9f4d97a414a02d9f78af7c523a7d
Pedro Enrique <pedro.tma@gmail.com>
2011-02-28 21:37:15 -0500

    first commit
    Signed-off-by: Pedro Enrique <pedro.tma@gmail.com>

---
+++ Resources/win6.js
@@ -0,0 +1,32 @@
+var win = Ti.UI.currentWindow;
+win.title='win6';
+
+var oldWindow = win.oldWindow;
+oldWindow.push(win);
+
+var button1 = Ti.UI.createButton({
+	title:'go to root without animation',
+	height:50,
+	width:250,
+	top:20
+});
+
+var button2 = Ti.UI.createButton({
+	title:'go to root with animation',
+	height:50,
+	width:200,
+	top:150
+});
+win.add(button1);
+win.add(button2);
+button1.addEventListener('click',function(){
+	for(var i = 0; i<oldWindow.length;i++){
+		Ti.UI.currentTab.close(oldWindow[i],{animated:false});
+	}
+});
+button2.addEventListener('click',function(){
+	for(var i = 0; i<win.oldWindow.length;i++){
+		Ti.UI.currentTab.close(oldWindow[i],{animated:false});
+	}
+	Ti.UI.currentTab.close(win,{animated:true});
+});
